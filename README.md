b28eb1201c1410bf8178267225e226f033a7ae6b
bsides44 <maddog.star@gmail.com>
2023-03-20 17:44:00 +1300

    click to play working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # VR-interactive-film
 Using A-frame to make a VR film interactive
+
+Local server:
+npm i
+npm start
+
+Build:
+npm run dist
---
+++ play-on-click.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent('play-on-click', {
+    schema: {
+      toPlay: {type: 'boolean', default: true}
+    },
+    init: function () {
+      this.onClick = this.onClick.bind(this);
+      window.addEventListener('click', this.onClick);
+    },
+    onClick: function (evt) {
+      console.log('click')
+      var videoEl = this.el.getAttribute('material').src;
+      // var videoEl = document.querySelector("#monoPico").components.material.material.map.image;
+      if (videoEl) {
+        this.el.object3D.visible = true;
+        if (this.data.toPlay) {
+          videoEl.play()
+          document.querySelector("#play").object3D.visible = false
+          document.querySelector("#pause").object3D.visible = true
+          this.data.toPlay = !this.data.toPlay
+        } else {
+          videoEl.pause()
+          document.querySelector("#play").object3D.visible = true
+          document.querySelector("#pause").object3D.visible = false
+          this.data.toPlay = !this.data.toPlay
+        }       
+      }
+      else return
+    },
+    remove: function () {
+      window.removeEventListener('click', this.onClick);
+    }
+  });
