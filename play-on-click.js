cb543ace60b4def597788a2b378f47384ceef9b3
bsides44 <maddog.star@gmail.com>
2023-03-21 21:06:41 +1300

    look controls and portal working

--- play-on-click.js
+++ play-on-click.js
@@ -1,32 +1,31 @@
 AFRAME.registerComponent('play-on-click', {
-    schema: {
-      toPlay: {type: 'boolean', default: true}
-    },
-    init: function () {
-      this.onClick = this.onClick.bind(this);
-      window.addEventListener('click', this.onClick);
-    },
-    onClick: function (evt) {
-      console.log('click')
-      var videoEl = this.el.getAttribute('material').src;
-      // var videoEl = document.querySelector("#monoPico").components.material.material.map.image;
-      if (videoEl) {
-        this.el.object3D.visible = true;
-        if (this.data.toPlay) {
-          videoEl.play()
-          document.querySelector("#play").object3D.visible = false
-          document.querySelector("#pause").object3D.visible = true
-          this.data.toPlay = !this.data.toPlay
-        } else {
-          videoEl.pause()
-          document.querySelector("#play").object3D.visible = true
-          document.querySelector("#pause").object3D.visible = false
-          this.data.toPlay = !this.data.toPlay
-        }       
+  schema: {
+    toPlay: { type: 'boolean', default: true }
+  },
+  init: function () {
+    this.onClick = this.onClick.bind(this);
+    window.addEventListener('click', this.onClick);
+  },
+  onClick: function (evt) {
+    var videoEl = this.el.getAttribute('material').src;
+    // var videoEl = document.querySelector("#monoPico").components.material.material.map.image;
+    if (videoEl) {
+      this.el.object3D.visible = true;
+      if (this.data.toPlay) {
+        videoEl.play()
+        document.querySelector("#play").object3D.visible = false
+        document.querySelector("#pause").object3D.visible = true
+        this.data.toPlay = !this.data.toPlay
+      } else {
+        videoEl.pause()
+        document.querySelector("#play").object3D.visible = true
+        document.querySelector("#pause").object3D.visible = false
+        this.data.toPlay = !this.data.toPlay
       }
-      else return
-    },
-    remove: function () {
-      window.removeEventListener('click', this.onClick);
     }
-  });
+    else return
+  },
+  remove: function () {
+    window.removeEventListener('click', this.onClick);
+  }
+});
